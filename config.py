bbdb0b72466e1d9b430956012bf2198007bd7976
pinge1638 <126724522+pinge1638@users.noreply.github.com>
2026-07-25 20:34:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,7 @@ ONEPIECE_TOPIC=os.getenv("ONEPIECE_TOPIC")
 GIVEAWAY_TOPIC=os.getenv("GIVEAWAY_TOPIC")
 NEWS_TOPIC=os.getenv("NEWS_TOPIC")
 ANNOUNCEMENT_TOPIC=os.getenv("ANNOUNCEMENT_TOPIC")
+print("BOT_TOKEN =", os.getenv("BOT_TOKEN"))
+print("OWNER_ID =", os.getenv("OWNER_ID"))
+
 OWNER_ID = int(os.getenv("OWNER_ID"))
