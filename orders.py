af207077cc224ccf2da63f2b8ba318397570270b
pinge1638 <126724522+pinge1638@users.noreply.github.com>
2026-08-08 00:33:00 +0800

    Update orders.py

--- orders.py
+++ orders.py
@@ -1,1 +1,40 @@
+from telegram import (
+    Update,
+)
 
+from telegram.ext import (
+    ContextTypes,
+)
+
+from database import (
+    approve_order,
+    reject_order,
+)
+async def approve(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    query = update.callback_query
+    await query.answer()
+
+    order_number = int(
+        query.data.replace("approve_", "")
+    )
+
+    approve_order(order_number)
+
+    await query.edit_message_caption(
+        caption=query.message.caption + "\n\n✅ APPROVED"
+    )
+  async def reject(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    query = update.callback_query
+    await query.answer()
+
+    order_number = int(
+        query.data.replace("reject_", "")
+    )
+
+    reject_order(order_number)
+
+    await query.edit_message_caption(
+        caption=query.message.caption + "\n\n❌ REJECTED"
+    )
